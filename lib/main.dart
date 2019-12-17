a0e548780b0ed36d90121b4af6fcc74e8c50f114
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2019-12-17 11:01:04 -0300

    starting with providers and listeners

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'screens/product_overview_screen.dart';
+import './providers/products_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shop App',
-      theme: ThemeData(
-        primaryColor: Color.fromRGBO(244, 143, 177, 1),
-        accentColor: Color.fromRGBO(128, 203, 196, 1),
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (ctx) => ProductsProvider(),
+      child: MaterialApp(
+        title: 'Shop App',
+        theme: ThemeData(
+          primaryColor: Color.fromRGBO(244, 143, 177, 1),
+          accentColor: Color.fromRGBO(128, 203, 196, 1),
+          fontFamily: 'Lato',
+        ),
+        home: ProductOverviewScreen(),
       ),
-      home: ProductOverviewScreen(),
     );
   }
 }
---
+++ lib/widgets/grid_product.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products_provider.dart';
+import './product_item.dart';
+
+class GridProduct extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<ProductsProvider>(context);
+    final products = productsData.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: products.length,
+      itemBuilder: (context, index){
+        return ProductItem(products[index].id, products[index].title, products[index].imageUrl);
+      },
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10
+      ),
+    );
+  }
+}
