8bd401dc363b6906e98999048edae31e0fb334d5
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2019-12-19 15:30:35 -0300

    starting product detail page

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -29,6 +29,34 @@ class ProductDetailScreen extends StatelessWidget {
           style: TextStyle(color: Colors.white),
         ),
       ),
+      body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(20),
+                height: 300,
+                width: double.infinity,
+                child: Image.network(
+                  _product.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(height: 10,),
+              Text('\$${_product.price}',
+              style: TextStyle(color: Colors.grey, fontSize: 20),
+              ),
+              SizedBox(height: 10,),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                width: double.infinity,
+                child: Text(_product.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+                ),
+              )
+            ],
+          ),
+      ),
     );
   }
 }
