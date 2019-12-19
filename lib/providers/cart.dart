5a7e06497cc6c40247259b862feadf1bdd5de4f0
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2019-12-18 22:33:36 -0200

    creating model cart

---
+++ lib/providers/cart.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+class CardItem{
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CardItem({
+    @required this.id,
+    @required this.title,
+    @required this.quantity,
+    @required this.price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CardItem> _items;
+
+  Map<String, CardItem> get items{
+    return {..._items};
+  }
+
+  void addItem(String productId, double price, String title,){
+    if(_items.containsKey(productId)){
+      _items.update(
+        productId,
+        (existingCardItem) => CardItem(
+          id: existingCardItem.id,
+          title: existingCardItem.title,
+          price: existingCardItem.price,
+          quantity: existingCardItem.quantity + 1,
+        )  
+        );
+    }else{
+      _items.putIfAbsent(
+        productId,
+        () => CardItem(id: DateTime.now().toString(),
+        title: title,
+        price: price,
+        quantity: 1
+        ));
+    }
+  }
+
+} 
