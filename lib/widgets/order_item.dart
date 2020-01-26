7adebe3f8ef0c814baaa52422ee7b52d29a52b29
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2020-01-26 12:30:24 -0300

    making orders expandable

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -2,10 +2,17 @@ import 'package:flutter/material.dart';
 import '../providers/orders.dart' as ord;
 import 'package:intl/intl.dart';
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   OrderItem(this.order);
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  bool _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -13,13 +20,34 @@ class OrderItem extends StatelessWidget {
       margin: EdgeInsets.all(10),
       child: Column(children: <Widget>[
         ListTile(
-          title: Text('\$${order.amount}'),
-          subtitle: Text(DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime)),
+          title: Text('\$${widget.order.amount}'),
+          subtitle: Text(DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime)),
           trailing:
-            IconButton(icon: Icon(Icons.expand_more),
-            onPressed:  (){},
+            IconButton(icon: Icon(!_expanded ? Icons.expand_more : Icons.expand_less),
+            onPressed:  (){
+              setState(() {
+                _expanded = !_expanded;
+              });
+            },
           ),
         ),
+        if(_expanded)
+          Container(
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.symmetric(vertical: 10),
+            height: 100,
+            child: ListView(children: <Widget>[
+              ...widget.order.products.map(
+                (product) => Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                      Text(product.title, style: TextStyle(fontSize: 18),),
+                      Text('${product.quantity}x\$${product.price}', style: TextStyle(fontSize: 18, color: Colors.grey),)
+                    ],
+                  )
+                ).toList(),
+            ],),
+          ),
       ],),
     );
   }
